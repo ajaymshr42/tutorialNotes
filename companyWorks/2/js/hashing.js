5e4e26d12852b689f70f4ab406ca0d3413b2f0b7
Ajay Mishra <ajay.mishra@nlpcaptcha.com>
2017-06-14 16:35:24 +0530

    added murmur hashing technique to generate hash key Using Murmur3 algorithm

---
+++ companyWorks/2/js/hashing.js
@@ -0,0 +1,12 @@
+document.write('lets start hashing this shit!<br>');
+
+function murmurHash_128(key,len,seed){
+	c1=0xcc9e2d52,c2=0x1b873593,r1=15,r2=13,m=5,n=0xe6546b64;
+	hash=seed;
+
+	b=1;
+	document.write(parseInt(b,10).toString(2));
+
+}
+
+murmurHash_128('abc',3,1);
